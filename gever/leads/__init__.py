e337ceb5729e14019c3053dc6d0f3b5e73cd7df4
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-26 19:43:07 -0400

    feat: export Lead Hunter domain types

---
+++ gever/leads/__init__.py
@@ -0,0 +1,19 @@
+from .models import (
+    LeadCandidate,
+    LeadClassification,
+    LeadRecord,
+    LeadStatus,
+    OpportunityType,
+    SearchRunSummary,
+)
+from .store import LeadStore
+
+__all__ = [
+    "LeadCandidate",
+    "LeadClassification",
+    "LeadRecord",
+    "LeadStatus",
+    "OpportunityType",
+    "SearchRunSummary",
+    "LeadStore",
+]
