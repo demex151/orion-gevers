13cacba150be25e2473fa4cc2e760457c3d176b4
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-25 23:10:47 -0400

    feat: render Figma-matched GEVER home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import HomeShell from './HomeShell.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomeShell />
   </StrictMode>,
 )
