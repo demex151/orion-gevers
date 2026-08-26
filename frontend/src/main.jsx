761a9810f0c3d57cecf096b21f1ebd6f2c889c5b
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-26 01:19:17 -0400

    feat: enable visual subtitle and conversation mirror

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './viewportScale.js'
+import './visualMirror.js'
 import HomeShell from './HomeShell.jsx'
 
 createRoot(document.getElementById('root')).render(
