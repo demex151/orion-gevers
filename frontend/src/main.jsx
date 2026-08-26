ca8c341fdcaa8747934c824f0213d302a5bea732
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-26 01:30:55 -0400

    fix: remove DOM polling mirror from GEVER runtime

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './viewportScale.js'
-import './visualMirror.js'
 import HomeShell from './HomeShell.jsx'
 
 createRoot(document.getElementById('root')).render(
