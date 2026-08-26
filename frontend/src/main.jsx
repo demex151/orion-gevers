2df5020b7cfbc6dfd6f9ae8743ccc244ac07b7f4
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-26 12:19:16 -0400

    fix: prevent StrictMode from disabling HomeShell session polling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './viewportScale.js'
@@ -7,7 +6,5 @@ import './safeVisualBridge.js'
 import HomeShell from './HomeShell.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomeShell />
-  </StrictMode>,
+  <HomeShell />,
 )
