3ce7bf1ce58fc1fb8489030b8bad642837199fb2
demex151 <jimenezalvarezjose32@gmail.com>
2026-08-26 16:08:15 -0400

    fix: prevent duplicate voice controller in development

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './viewportScale.js'
@@ -6,7 +5,5 @@ import './visualMirror.js'
 import HomeShell from './HomeShell.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomeShell />
-  </StrictMode>,
+  <HomeShell />,
 )
